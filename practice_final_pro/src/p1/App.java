4fc4746fc7af9f0ba320bfc80f8e0ff736d9ebb2
rinku <54875424+sarkr72@users.noreply.github.com>
2019-12-03 21:29:17 -0500

    Add files via upload

--- practice_final_pro/src/p1/App.java
+++ practice_final_pro/src/p1/App.java
@@ -3,47 +3,79 @@ package p1;
 import java.util.Scanner;
 
 public class App {
-	public   static Scanner input = new Scanner(System.in);
-	public static String dLine = "----------------------------------";
+	public static Scanner input = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
+		int x = 5;
+		// class is a data type
+		Customer c1 = new Customer("John", "Doe", "6312029454", "986456841687", 1000.00);
+		System.out.printf("%-8s%8s%12s%6s%10.2f\n", c1.getAccountNumber(),
+													c1.getFirstName(),
+													c1.getLastName(),
+													c1.getPhone(),
+													c1.getBalance());
+
+		Customer c2 = new Customer("cena", "Doe", "6311234567", "46546548847", 100000.00);
+		System.out.printf("%-8s%8s%12s%10s%10.2f\n", c2.getAccountNumber(),
+													c2.getFirstName(),
+													c2.getLastName(),
+													c2.getPhone(),
+													c2.getBalance());
 		
-//		int x = 5;
-//		// class is a data type
-//		Customer c1 = new Customer("John", "Doe", "6312029454", "986456841687", 1000.00);
-////		System.out.printf("%-8s%8s%12s%10s%10.2f\n", c1.getAccountnumber(),
-////													c1.getFirstName(),
-////													c1.getLastName(),
-////													c1.getPhone(),
-////													c1.getBalance());
-//		
-//		Customer c2 = new Customer("John", "Doe", "6311234567", "46546548847", 100000.00);
-////		System.out.printf("%-8s%8s%12s%10s%10.2f\n", c2.getAccountnumber(),
-////													c2.getFirstName(),
-////													c2.getLastName(),
-////													c2.getPhone(),
-////													c2.getBalance());
-////		
-////		System.out.println(c1);
-////		System.out.println(c2);
+//		System.out.println(c1);
+//		System.out.println(c2);
 //		// array of data type
 //		Customer[] customers = new Customer[5];
 //		customers[0] = c1;
 //		customers[1] = c2;
 //		System.out.println(customers[0]);
-//		
+
 		int count = 0;
 		Customer[] customers = new Customer[10000];
-		while (count < 2) {
-			Customer c1 = createAccount();
-			customers[count++] = c1;
+		while (count < 3) {
+			Customer c3 = createAccount();
+			customers[count++] = c3;
 		}
 		System.out.println();
-		
-		for(int i = 0; i < count; i++) {
+
+		for (int i = 0; i < count; i++) {
 			System.out.println(customers[i]);
 		}
-}
+
+		System.out.println();
+		System.out.println("Welcome to TD Bank ATM");
+		System.out.println("enter your account Number: ");
+		String accNum = input.next();
+		
+			for (int i = 0; i < count; i++) {
+				if (accNum.matches(customers[i].getAccountNumber())) {
+					while (true) {
+						System.out.println("choose one:");
+					showMenu();
+					int n = input.nextInt();
+					if (n == 1) {
+						System.out.println("balance is: " + customers[i].getBalance());
+					} else if (n == 2) {
+						customers[i].getDeposit(customers[i].getBalance());	
+					} else if (n == 3) {
+						customers[i].getWithdrawal(customers[i].getBalance());
+					} else if (n == 4) {
+						break;
+					}
+				}
+			}
+		}
+			
+		System.out.println("Which account do you want to delete?\n'0' for 1st, '1' for 2nd, '2' for 3rd  so on.... :");
+		int delete = input.nextInt();
+		customers[delete].close();
+
+		for (int i = 0; i < count; i++) {
+			System.out.println(customers[i]);
+		}
+	}
 	
 	public static Customer createAccount() {
 		System.out.println("enter first name: ");
@@ -58,55 +90,16 @@ public class App {
 		double balance = Double.parseDouble(input.next());
 		Customer c = new Customer(fName, lName, phone, accountNumber, balance);
 		System.out.println(c);
-		getInterest(balance);
-		getDeposit(balance);
-		getWithdrawal(balance);
+		c.getInterest(balance);
+		c.checkDeposit();
+		System.out.println();
+		c.checkWithdraw();
 		return c;
 	}
 	
-	public static void getReceipt() {
-		System.out.println("\tTD Bank\n\t474 PORTION RD\n\tLAKE RONKONKOMA, NY, USA");
-		System.out.println(dLine);
-		System.out.println("account number:\t\t" + Customer.getAccountNumber() + "\n" + "Name:\t\t" + Customer.getFirstName() 
-							+ " " + Customer.getLastName());
-	
+	public static void showMenu() {
+		System.out.println("Main Menu");
+		System.out.println("1.check balance: \n" + "2.deposit \n" + "3.withdraw \n" + "4.exit");
 	}
-	
-	public static void getWithdrawal(double balance) {
-		System.out.print("Enter the withdrawal amount: ");
-		double withdraw = input.nextDouble();
-		if(withdraw > balance) {
-			System.out.println("Sorry, can't process transaction");
-		}else {
-		balance -= withdraw;
-		System.out.println("Balance after withdrawal " + balance);
-		System.out.println(dLine);
-		getReceipt();
-		System.out.println("Withdrawal:\t\t" + withdraw);
-		System.out.println("Available balance:\t" + balance);
-		System.out.println(dLine);
-		}
-	}
-	public static void getDeposit(double balance) {
-		System.out.print("enter the deposit: ");
-		double deposit = input.nextDouble();
-		if(deposit <= 0) {
-			System.out.println("Sorry, can't process transaction");
-		}else {
-		balance += deposit;
-		System.out.println("balance after deposit: " + balance);
-		System.out.println(dLine);
-		getReceipt();
-		System.out.println("Deposit:\t\t" + deposit);
-		System.out.println("Available balance:\t" + balance);
-		System.out.println(dLine);
-		}
-	}
-	
-	public static void getInterest(double balance) {
-		double rate = 0.123;
-		double r = balance * rate;
-		balance += r;
-		System.out.println("balance with interest: " + balance);
-	}
+
 }
--- practice_final_pro/src/p1/Customer.java
+++ practice_final_pro/src/p1/Customer.java
@@ -3,13 +3,14 @@ package p1;
 import java.util.Scanner;
 
 public class Customer {
- public static Scanner input = new Scanner(System.in);
-	static String lastName;
-	static String firstName;
-	String phone;
-	static String accountNumber;
-	double balance;
-	
+	public static String dLine = "----------------------------------";
+	public static Scanner input = new Scanner(System.in);
+	private String lastName;
+	private String firstName;
+	private String phone;
+	private String accountNumber;
+	private double balance;
+
 	public Customer(String firstName, String lastName, String phoneNumber, String accountNumber, double balance) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -17,31 +18,136 @@ public class Customer {
 		this.accountNumber = accountNumber;
 		this.balance = balance;
 	}
-	
-	
-	
-	public static String getFirstName() {
+
+	public void checkDeposit() {
+		System.out.print("Enter the date: ");
+		String date = input.next();
+		System.out.print("Enter the deposit: ");
+		double deposit = input.nextDouble();
+		System.out.println(dLine);
+		System.out.println( "       Check Deposit");
+		System.out.println("Deposit to the account of\nNAME :" + firstName + " " + lastName + "                  CASH");
+		System.out.println("DATE :" + date + "\n      TD Bank\nAmerica's most convienent bank        SUBTOTAL :"+ deposit
+				+ "\n" + "Account Number *:" + accountNumber + "          $" + deposit);
+		balance += deposit;
+		System.out.println("balance after deposit: " + balance);
+		System.out.println(dLine);
+		getReceipt();
+		System.out.println("Deposit:\t\t" + deposit);
+		System.out.println("Available balance:\t" + balance);
+		System.out.println(dLine);
+	}
+
+	public void checkWithdraw() {
+		System.out.println("WITHDRAW");
+		System.out.print("Enter the date: ");
+		String date = input.next();
+		System.out.print("Enter the withDraw: ");
+		double withDraw = input.nextDouble();
+		System.out.print("Write out the amount in words:");
+		String amount = input.next();
+		System.out.print("for?");
+		String For = input.next();
+		System.out.print("signature: ");
+		String signature = input.next();
+		System.out.print("Enter the routhing number: ");
+		String routing = input.next();
+		System.out.println(dLine);
+		System.out.println("TD Bank\n\t474 PORTION RD\n\tLAKE RONKONKOMA, NY-11779, USA");
+		System.out.println("                                    " + date);
+		System.out.println("pay to the\n" + "Order of: " + firstName + " " + lastName + "            $"
+				+ withDraw);
+		System.out.printf("%-20s%10s\n", amount, "dollars");
+		System.out.println("TD BANK\nAmerica's most convienent bank." + "                 for" + For
+				+ "        "+signature);
+		System.out.print(routing);
+		System.out.println("    " + accountNumber);
+		balance -= withDraw;
+		System.out.println("Balance after withdrawal " + balance);
+		System.out.println(dLine);
+		getReceipt();
+		System.out.println("Withdrawal:\t\t" + withDraw);
+		System.out.println("Available balance:\t" + balance);
+		System.out.println(dLine);
+	}
+
+	public void getReceipt() {
+		System.out.println("\tTD Bank\n\t474 PORTION RD\n\tLAKE RONKONKOMA, NY, USA");
+		System.out.println(dLine);
+		System.out.println("account number:\t\t" + accountNumber + "\n" + "Name:\t\t" + firstName + " " + lastName);
+	}
+
+	public void getWithdrawal(double balance) {
+		System.out.print("Enter the withdrawal amount: ");
+		double withdraw = input.nextDouble();
+		if (withdraw > balance || withdraw < 0) {
+			System.out.println("Sorry, can't process the transaction");
+		} else {
+			balance -= withdraw;
+			System.out.println("Balance after withdrawal " + balance);
+			System.out.println(dLine);
+			getReceipt();
+			System.out.println("Withdrawal:\t\t" + withdraw);
+			System.out.println("Available balance:\t" + balance);
+			System.out.println(dLine);
+		}
+	}
+
+	public void getDeposit(double balance) {
+		System.out.print("enter the deposit: ");
+		double deposit = input.nextDouble();
+		if (deposit <= 0) {
+			System.out.println("Sorry, can't process the transaction");
+		} else {
+			balance += deposit;
+			System.out.println("balance after deposit: " + balance);
+			System.out.println(dLine);
+			getReceipt();
+			System.out.println("Deposit:\t\t" + deposit);
+			System.out.println("Available balance:\t" + balance);
+			System.out.println(dLine);
+		}
+	}
+
+	public void getInterest(double balance) {
+		System.out.println("how many days?");
+		int days = input.nextInt();
+		double rate = 0.02;
+		double r = balance * rate / 365 * days;
+		balance += r;
+		System.out.println("balance with interest: " + balance);
+	}
+
+	public String getFirstName() {
 		return firstName;
 	}
-	
-	public static String getLastName() {
+
+	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public String getPhone() {
 		return phone;
 	}
-	
-	public static String getAccountNumber() {
+
+	public String getAccountNumber() {
 		return accountNumber;
 	}
-	
+
 	public double getBalance() {
 		return balance;
 	}
-	
+
 	public String toString() {
 		return (accountNumber + " " + firstName + " " + lastName + " " + phone + " " + balance);
 	}
-	
+
+	public void close() {
+		firstName = "closed";
+		lastName = "--";
+		accountNumber = "--";
+		balance = 0;
+		phone = "--";
+	}
+
 }
