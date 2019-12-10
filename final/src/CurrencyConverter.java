e7cde7cb43a2ecc9eb65a4219c000c124a98d698
rinku <54875424+sarkr72@users.noreply.github.com>
2019-12-10 11:14:20 -0500

    Add files via upload

--- final/src/CurrencyConverter.java
+++ final/src/CurrencyConverter.java
@@ -1,18 +1,42 @@
 
 public class CurrencyConverter {
 
-	private static String single_digits[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-
+	private static String single_digits[] = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight",
+			"nine" };
+	private static String[] twoDigits = { "ten", "eleven", "twelve", "thirten", "fourten", "fiften", "sixten",
+			"seventen", "eighten", "nineten" };
+	private static String[] ten_multiples] = {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+	
 	public static void main(String[] args) {
-		System.out.println(convertCurrency(3.12));
+		System.out.println(convertCurrency(23.12));
 //		System.out.println(getWholePart(123.14));
 //		System.out.println(getFractionPart(123.14));
 	}
 
+	private static String convertDoubleDigits(String wholePart) {
+		int amount = Integer.parseInt(wholePart);
+		if (amount <= 9) {
+			return convertSingleDigit(wholePart);
+		}
+		if (amount / 20 == 0) {// between 10-19
+			return twoDigits[amount % 10];
+		}
+		
+		String onesDigit = wholePart.substring(1);
+		String tensDigit = wholePart.substring(0, 1);
+		
+		if(onesDigit.contentEquals("0")) {
+			return ten_multiples[Integer.parseInt(tensDigit)];
+		}else {
+			return ten_multiples[Integer.parseInt(tensDigit)] + " " + single_digits[Integer.parseInt(onesDigit)];
+	}
+		return null;
+		}
+
 	public static String convertCurrency(double amount) {
 		String wholePart = getWholePart(amount);
 		String fraction = getFractionPart(amount);
-		String wholePartInWords = convertSingleDigit(wholePart);
+		String wholePartInWords = convertDoubleDigits(wholePart);
 		String amountInWords = wholePartInWords + " and " + fraction + "/100";
 		return amountInWords;
 	}
